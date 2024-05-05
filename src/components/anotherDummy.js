57e013d2b4394000f8ea41e63be9dd7475f36763
NatsuDrag112 <vilesh_rajbelly@thbs.com>
2024-05-05 14:12:05 +0530

    5-5-2024 1411

--- src/components/anotherDummy.js
+++ src/components/anotherDummy.js
@@ -1,32 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
+function YourComp() {
+  const [userData, setUserData] = useState([]);
 
+//   useEffect(() => {
+//     axios.get('http://localhost:5000/api/data')
+//       .then(function (response) {
+//         setUserData(response.data);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   }, []);
 
+  return (
+    <div className='pt-16 text-center'>
+      <button className='bg-blue-500 text-white px-4 py-1 rounded-md' onClick={handleClick}>Fetch Data</button>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        {userData.map(user => (
+          <Card key={user.email} className='mx-2' >
+          <Card.Img variant="top" src={user.image} alt='not found'/>
+            <Card.Body className='border-2 rounded-lg'>
+              <Card.Title className='border-b '>{user.firstName} {user.lastName}</Card.Title>
+              <Card.Text>Email: {user.email}</Card.Text>
+              <Card.Text className='border-t'>password: {user.password}</Card.Text>
+              {/* You may add more user details here */}
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 
-function YourComp(){
-    const [res, setRes] = useState('')
-    const handleclick = () => {
-        console.log('clicked')
-        axios.get('http://localhost:5000/api/data')
-        .then(function (response) {
-            const responseData = response.data
-            console.log(responseData.length)
-            console.log(responseData)
-            console.log(responseData[2].email)
-        })
-    }
-    // useEffect(()=>{
-    //     axios.get('http://localhost:5000/api/data')
-    //     .then(res => console.log(res.data))
-    //     .catch(err => console.log('error caught'+err))
-    // }, [])
-    return (
-        <div className='pt-16 text-center'>
-            <button className='bg-blue-500 text-white px-4 py-1 rounded-md' onClick={handleclick}>click me</button>
-            <p id='json'>json data</p>
-        </div>
-    )
+  function handleClick() {
+    console.log('clicked');
+    axios.get('http://localhost:5000/api/data')
+      .then(function (response) {
+        setUserData(response.data);
+        console.log(userData[1].image);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
 
-export default YourComp
+export default YourComp;
---
+++ src/components/anotherDummyBU.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
+
+function YourComp(){
+    const [res, setRes] = useState('')
+    const handleclick = () => {
+        console.log('clicked')
+        axios.get('http://localhost:5000/api/data')
+        .then(function (response) {
+            setRes(response.data) 
+            console.log(res.length)
+            console.log(res)
+            console.log(res[2].email)
+        })
+    }
+    // useEffect(()=>{
+    //     axios.get('http://localhost:5000/api/data')
+    //     .then(res => console.log(res.data))
+    //     .catch(err => console.log('error caught'+err))
+    // }, [])
+    return (
+        <div className='pt-16 text-center'>
+            <button className='bg-blue-500 text-white px-4 py-1 rounded-md' onClick={handleclick}>click me</button>
+            <p id='json'>json data</p>
+        </div>
+    )
+}
+
+export default YourComp
