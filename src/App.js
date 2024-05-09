76b7fa00e342802ec00f50d5f5ccf89cd4970e83
NatsuDrag112 <vilesh_rajbelly@thbs.com>
2024-05-09 12:44:06 +0530

    fdaz

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ import UserSignup from './pages/userSignup';
 import ForgotPassword from './pages/forgotPass';
 import UserDash from './pages/userDashboard';
 import RestaurantSignin from './pages/restaurantSignIn';
+import RestaurantSignup from './pages/restaurantSignup';
 import RestaurantDash from './pages/restaurantDashboard';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import React from "react";
-import Home from "./pages/home";
-import UserSignin from "./pages/userSignin";
-import UserSignup from "./pages/userSignup";
-import ForgotPassword from "./pages/forgotPass";
-import UserDash from "./pages/userDashboard";
-import RestaurantSignin from "./pages/restaurantSignIn";
-import RestaurantSignup from "./pages/restaurantSignup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -26,6 +18,7 @@ function App() {
           <Route path="/home" element={<Home/>}/>
           <Route path="/usersignin" element={<UserSignin/>}/>
           <Route path="/restaurantsignin" element={<RestaurantSignin/>}/>
+          <Route path="/restaurantsignup" element={<RestaurantSignup/>}/>
           <Route path="/usersignup" element={<UserSignup/>}/>
           <Route path="/forgotpassword" element={<ForgotPassword/>}/>
           <Route path="/userdashboard" element={<UserDash/>}/>
